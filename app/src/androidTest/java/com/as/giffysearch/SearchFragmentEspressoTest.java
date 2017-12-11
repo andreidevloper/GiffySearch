0cd24d7bd1f219faa1558a3b20b3b5c5def2126a
andreidevloper <andreidevloper@gmail.com>
2017-12-11 18:29:21 +0200

    Testing. SearchFragmentEspressoTest.java: 1)Add new test that does two searches without failed responses. 2)Add new test that does two searches with failed responses.

---
+++ app/src/androidTest/java/com/as/giffysearch/SearchFragmentEspressoTest.java
@@ -0,0 +1,221 @@
+package com.as.giffysearch;
+
+import android.support.test.espresso.NoMatchingViewException;
+import android.support.test.espresso.action.ViewActions;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+
+import com.as.giffysearch.Adapters.SearchResultRecyclerViewAdapter;
+import com.as.giffysearch.Controllers.PaggingController;
+import com.as.giffysearch.Fragments.SearchFragment;
+import com.as.giffysearch.ThirdParty.Giphy.GiphyAPI;
+
+import static junit.framework.Assert.fail;
+
+/**
+ * Created by Andrejs Skorinko on 12/8/2017.
+ *
+ */
+
+@RunWith(Parameterized.class)
+@LargeTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class SearchFragmentEspressoTest
+{
+    private static final String LOG_TAG = SearchFragmentEspressoTest.class.getSimpleName();
+
+    @Parameterized.Parameters
+    public static List<Object[]> data() {
+        return Arrays.asList(new Object[1][0]);
+    }
+
+    public SearchFragmentEspressoTest() {
+    }
+
+    private SearchFragment searchFragment_;
+
+    // Max request's time for sending and receiving GIFs from GIPHY
+    private static int MAX_REQUEST_TIME = 800;
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() throws Exception
+    {
+        //Before Test case execution
+        if(BuildConfig.BUILD_TYPE.equals("debug"))
+        {
+            MAX_REQUEST_TIME = 3000;
+        }
+        else if(BuildConfig.BUILD_TYPE.equals("releaseWithTest"))
+        {
+            MAX_REQUEST_TIME = 2000;
+        }
+    }
+
+    @Test
+    public void test1SearchFragment()
+    {
+        onView(withId(R.id.first_search_view)).perform(ViewActions.click());
+
+        searchGif("pigtail");
+        searchGif("diggy");
+    }
+
+    @Test
+    public void test2ManuallyFailedResponseSearchFragment()
+    {
+        onView(withId(R.id.first_search_view)).perform(ViewActions.click());
+
+        GiphyAPI.INSTANCE.setTesting(true);
+
+        searchGif("pigtail");
+        searchGif("diggy");
+    }
+
+    private void scrollUpRecyclerView(SearchResultRecyclerViewAdapter adapter)
+    {
+        int halfSize = adapter.getItemCount() / 2 - 4;
+        for(int i = halfSize; i < adapter.getItemCount(); i++)
+        {
+            onView(withId(R.id.search_result_recycler_view)).perform(RecyclerViewActions.scrollToPosition(i));
+        }
+    }
+
+    private void scrollDownRecyclerView(SearchResultRecyclerViewAdapter adapter)
+    {
+        int halfSize = adapter.getItemCount() / 2 + 4;
+        for(int i = halfSize; i >= 0; i--)
+        {
+            onView(withId(R.id.search_result_recycler_view)).perform(RecyclerViewActions.scrollToPosition(i));
+        }
+    }
+
+    private void searchGif(String name)
+    {
+        onView(withId(R.id.search_bar_edit_view))
+                .perform(ViewActions.clearText());
+
+        onView(withId(R.id.search_bar_edit_view))
+                .perform(ViewActions.typeText(name));
+
+        onView(withId(R.id.search_bar_edit_view))
+                .perform(pressImeActionButton())
+                .check(matches(withText(getSearchBarQuery())));
+
+        searchFragment_ = getSearchFragment();
+        if(searchFragment_ != null)
+        {
+            PaggingController paggingController = searchFragment_.getPaggingController();
+
+            boolean isLoading = true;
+            while(isLoading)
+            {
+                isLoading = paggingController.isLoading();
+            }
+
+            long requestTime = paggingController.getLastRequestTime();
+            /*if(requestTime < MAX_REQUEST_TIME)
+            {
+                Debugging.logClass(Log.INFO, LOG_TAG, "Can search for " + MAX_REQUEST_TIME +
+                                                               " ms GIFs from GIPHY service ! " +
+                                                               " Current requestTime: " + requestTime);
+            }
+            else
+            {
+                fail("Couldn't search for " + MAX_REQUEST_TIME +
+                     " ms GIFs from GIPHY service ! " +
+                     " Current requestTime: " + requestTime);
+            } */
+
+            SearchResultRecyclerViewAdapter adapter = searchFragment_.getSearchResultRecyclerViewAdapter();
+
+            boolean isReachEnd = false;
+            while(!isReachEnd)
+            {
+                scrollUpRecyclerView(adapter);
+                isReachEnd = paggingController.isReachedEnd();
+                checkIsResponseFailed();
+            }
+
+            onView(withId(R.id.search_result_recycler_view)).perform(RecyclerViewActions.scrollToPosition(adapter.getItemCount() - 1));
+
+            boolean isReachTop = false;
+            while(!isReachTop)
+            {
+                scrollDownRecyclerView(adapter);
+                isReachTop = paggingController.isReachedTop();
+                checkIsResponseFailed();
+            }
+/*
+            isReachEnd = false;
+            while(!isReachEnd)
+            {
+                scrollUpRecyclerView(adapter);
+                isReachEnd = paggingController.isReachedEnd();
+                checkIsResponseFailed();
+            }*/
+        }
+        else
+        {
+            fail("Couldn't find SearchFragment!");
+        }
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+        //After Test case Execution
+    }
+
+    private SearchFragment getSearchFragment()
+    {
+        MainActivity activity = (MainActivity)UtilsEspressoTest.getActivityInstance();
+        return activity.getSearchFragment();
+    }
+
+    private String getSearchBarQuery()
+    {
+        MainActivity activity = (MainActivity)UtilsEspressoTest.getActivityInstance();
+        SearchFragment searchFragment = activity.getSearchFragment();
+        if(searchFragment != null)
+        {
+            return searchFragment.getSearchQuery();
+        }
+        return "";
+    }
+
+    private void checkIsResponseFailed()
+    {
+        try
+        {
+            // View is in hierarchy
+            onView(withId(R.id.try_again_button)).perform(ViewActions.click());
+        }
+        catch (NoMatchingViewException e)
+        {
+            // View is not in hierarchy
+        }
+    }
+}
---
+++ app/src/androidTest/java/com/as/giffysearch/UtilsEspressoTest.java
@@ -0,0 +1,89 @@
+package com.as.giffysearch;
+
+import android.app.Activity;
+import android.support.test.espresso.PerformException;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.util.HumanReadables;
+import android.support.test.espresso.util.TreeIterables;
+import android.support.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
+import android.support.test.runner.lifecycle.Stage;
+import android.view.View;
+
+import org.hamcrest.Matcher;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.concurrent.TimeoutException;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by Andrejs Skorinko on 12/8/2017.
+ *
+ */
+
+public class UtilsEspressoTest
+{
+    // source - https://stackoverflow.com/questions/24517291/get-current-activity-in-espresso-android
+    public static Activity getActivityInstance()
+    {
+        final Activity[] currentActivity = {null};
+
+        getInstrumentation().runOnMainSync(new Runnable()
+        {
+            public void run()
+            {
+                Collection<Activity> resumedActivity = ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(Stage.RESUMED);
+                Iterator<Activity> it = resumedActivity.iterator();
+                currentActivity[0] = it.next();
+            }
+        });
+
+        return currentActivity[0];
+    }
+
+    // source - https://stackoverflow.com/questions/21417954/espresso-thread-sleep
+    public static ViewAction waitId(final int viewId, final long millis) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isRoot();
+            }
+
+            @Override
+            public String getDescription() {
+                return "wait for a specific view with id <" + viewId + "> during " + millis + " millis.";
+            }
+
+            @Override
+            public void perform(final UiController uiController, final View view) {
+                uiController.loopMainThreadUntilIdle();
+                final long startTime = System.currentTimeMillis();
+                final long endTime = startTime + millis;
+                final Matcher<View> viewMatcher = withId(viewId);
+
+                do {
+                    for (View child : TreeIterables.breadthFirstViewTraversal(view)) {
+                        // found view with required ID
+                        if (viewMatcher.matches(child)) {
+                            return;
+                        }
+                    }
+
+                    uiController.loopMainThreadForAtLeast(50);
+                }
+                while (System.currentTimeMillis() < endTime);
+
+                // timeout happens
+                throw new PerformException.Builder()
+                        .withActionDescription(this.getDescription())
+                        .withViewDescription(HumanReadables.describe(view))
+                        .withCause(new TimeoutException())
+                        .build();
+            }
+        };
+    }
+}
