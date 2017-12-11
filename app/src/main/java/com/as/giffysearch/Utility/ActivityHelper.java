5ca373d93d701c2434d6bb9c76c351c4dfab01f2
andreidevloper <andreidevloper@gmail.com>
2017-12-11 17:22:31 +0200

    Debugging.java: 1)Add new Profiler class in order to measure elapsed time; 2)Add new static method (logClassThrowable) for logging with exception . ActivityHelper.java: 1)Add new helper methods: showKeyboard(...) and isNetworkAvailable(...).

--- app/src/main/java/com/as/giffysearch/Utility/ActivityHelper.java
+++ app/src/main/java/com/as/giffysearch/Utility/ActivityHelper.java
@@ -1,10 +1,14 @@
 package com.as.giffysearch.Utility;
 
-import android.app.Activity;
 import android.content.Context;
-import android.util.Log;
+
+import android.net.NetworkInfo;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+
+import android.net.ConnectivityManager;
+
+import android.util.Log;
 
 import com.as.giffysearch.MainActivity;
 
@@ -15,54 +19,72 @@ import com.as.giffysearch.MainActivity;
 
 public class ActivityHelper
 {
-    public static boolean hideKeyboard(Context context, Activity activity, int flag)
+    public static void hideKeyboard(Context applicationContext, View view)
     {
-        if(context == null || activity == null)
+        if(view == null || applicationContext == null)
         {
-            Debugging.logClass(Log.ERROR, MainActivity.LOG_TAG, "Can't hide keybaord. Context or/and activity are null !");
-            return false;
+            Debugging.logClass(Log.ERROR, MainActivity.LOG_TAG, "Can't hide keyboard. View or/and applicationContext are null !");
+            return;
         }
 
-        InputMethodManager inputManager = (InputMethodManager)context.getSystemService(Context.INPUT_METHOD_SERVICE);
-
-        View currentFocus = activity.getCurrentFocus();
-        if(currentFocus != null)
+        InputMethodManager inputManager = (InputMethodManager)applicationContext.getSystemService(Context.INPUT_METHOD_SERVICE);
+        if(inputManager != null)
         {
-            if(!inputManager.hideSoftInputFromWindow(currentFocus.getWindowToken(), flag))
+            if(!inputManager.hideSoftInputFromWindow(view.getWindowToken(), 0))
             {
                 Debugging.logClass(Log.ERROR, MainActivity.LOG_TAG, "Failed to hide keyboard !");
-                return false;
-            }
-            else
-            {
-                return true;
             }
         }
         else
         {
-            Debugging.logClass(Log.WARN, MainActivity.LOG_TAG, "Can't hide keybaord. Nothing has focus !");
-            return false;
+            Debugging.logClass(Log.ERROR, MainActivity.LOG_TAG, "Failed to hide keyboard: InputMethodManager is null !");
         }
     }
 
-    public static boolean hideKeyboard(Activity activity, View view, int flag)
+    public static void showKeyboard(Context applicationContext)
     {
-        if(view == null || activity == null)
+        if(applicationContext == null)
         {
-            Debugging.logClass(Log.ERROR, MainActivity.LOG_TAG, "Can't hide keybaord. View or/and activity are null !");
-            return false;
+            Debugging.logClass(Log.ERROR, MainActivity.LOG_TAG, "Can't hide keyboard. View or/and applicationContext are null !");
+            return;
         }
 
-        InputMethodManager inputManager = (InputMethodManager)activity.getSystemService(Context.INPUT_METHOD_SERVICE);
-
-        if(!inputManager.hideSoftInputFromWindow(view.getWindowToken(), flag))
+        InputMethodManager inputManager = (InputMethodManager)applicationContext.getSystemService(Context.INPUT_METHOD_SERVICE);
+        if(inputManager != null)
         {
-            Debugging.logClass(Log.ERROR, MainActivity.LOG_TAG, "Failed to hide keyboard !");
-            return false;
+            inputManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
         }
         else
         {
-            return true;
+            Debugging.logClass(Log.ERROR, MainActivity.LOG_TAG, "Failed to show keyboard: InputMethodManager is null ! ");
+        }
+    }
+
+    public static boolean isNetworkAvailable(Context applicationContext)
+    {
+        if(applicationContext == null)
+        {
+            Debugging.logClass(Log.ERROR, MainActivity.LOG_TAG, "Can't check network availability. ApplicationContext is null !");
+            return false;
+        }
+        ConnectivityManager manager = (ConnectivityManager)applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(manager != null)
+        {
+            NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+            if(networkInfo != null)
+            {
+                return networkInfo.isConnected();
+            }
+            else
+            {
+                Debugging.logClass(Log.ERROR, MainActivity.LOG_TAG, "Failed to check network availability: NetworkInfo is null!");
+                return false;
+            }
+        }
+        else
+        {
+            Debugging.logClass(Log.ERROR, MainActivity.LOG_TAG, "Failed to check network availability: ConnectivityManager is null!");
+            return false;
         }
     }
 }
--- app/src/main/java/com/as/giffysearch/Utility/Debugging.java
+++ app/src/main/java/com/as/giffysearch/Utility/Debugging.java
@@ -9,6 +9,91 @@ import android.util.Log;
 
 public class Debugging
 {
+    public static class Profiler
+    {
+        private long startTime_;
+        private long endTime_;
+        private long diffTime_;
+
+        private long totalDiffTime_;
+        private long counter_;
+
+        public void startTimer()
+        {
+            startTime_ = System.nanoTime();
+        }
+
+        public void stopTimer()
+        {
+            endTime_ = System.nanoTime();
+            diffTime_ = endTime_ - startTime_;
+
+            totalDiffTime_ += diffTime_;
+            counter_++;
+        }
+
+        // in milliseconds
+        public long getDiffTime()
+        {
+            return (diffTime_ / 1000000);
+        }
+
+        // in milliseconds
+        private long getAverageDiffTime() { if(counter_ != 0) return ((totalDiffTime_ / 1000000) / counter_); else return -1; }
+
+        public void logDiffTime(String logTag)
+        {
+            Debugging.logClass(Log.INFO, logTag, "Elapsed miliseconds: " + ((diffTime_ /1000000) + " ms."));
+        }
+
+        public void logAverageDiffTime(String logTag)
+        {
+            Debugging.logClass(Log.INFO, logTag, "Average diff time: " + getAverageDiffTime() + " ms. " +
+                                                          " For " + counter_ + " requests.");
+        }
+    }
+
+    public static void logClassThrowable(int level, String logTag, String outputText, Throwable exception)
+    {
+        switch (level)
+        {
+            case Log.DEBUG:
+            {
+                Log.d(logTag, outputText, exception);
+                break;
+            }
+
+            case Log.INFO:
+            {
+                Log.i(logTag, outputText, exception);
+                break;
+            }
+            case Log.WARN:
+            {
+                Log.w(logTag, outputText, exception);
+                break;
+            }
+
+            case Log.ERROR:
+            {
+                Log.e(logTag, outputText, exception);
+                break;
+            }
+
+            case Log.VERBOSE:
+            {
+                Log.v(logTag, outputText, exception);
+                break;
+            }
+
+            default:
+            {
+                Log.v(logTag, outputText, exception);
+                break;
+            }
+        }
+    }
+
     public static void logClass(int level, String logTag, String outputText)
     {
         switch (level)
